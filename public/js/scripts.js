6dc6024704cd697033033f4aaf1ff25c5ac9687f
samh99474 <samh99474@gmail.com>
2020-12-10 17:57:47 +0800

    admin_airCon_Gauge

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -59,4 +59,158 @@
         interval: 3000
      })
 
+/////////////////////////////////////////Gauge Defult Opts Options
+     var opts_temperature = {
+		// options here
+
+		// color configs
+		colorStart: "#6fadcf",
+		colorStop: void 0,
+		gradientType: 0,
+		strokeColor: "#e0e0e0",
+		generateGradient: true,
+		percentColors: [[0.0, "#a9d70b" ], [0.50, "#f9c802"], [1.0, "#ff0000"]],
+
+		// customize pointer
+		pointer: {
+			length: 0.8,
+			strokeWidth: 0.035,
+			iconScale: 1.0
+		  },
+
+		   // static labels
+		   staticLabels: {
+			font: "10px sans-serif",
+			labels: [15, 20, 25, 30, 50],
+			fractionDigits: 0
+		  },
+		   // static zones
+		   staticZones: [
+			{strokeStyle: "#3DAEF0", min: 0, max: 15},
+			{strokeStyle: "#397CE7", min: 15, max: 20},
+            {strokeStyle: "#30B32D", min: 20, max: 25},
+            {strokeStyle: "#FFDD00", min: 25, max: 30},
+            {strokeStyle: "#F03E3E", min: 30, max: 50},
+		  ],
+		   // render ticks
+		   renderTicks: {
+			divisions: 5,
+			divWidth: 1.1,
+			divLength: 0.7,
+			divColor: "#333333",
+			subDivisions: 3,
+			subLength: 0.5,
+			subWidth: 0.6,
+			subColor: "#666666"
+		  },
+	  
+		  // the span of the gauge arc
+		  angle: -0.05,
+	  
+		  // line thickness
+		  lineWidth: 0.44,
+	  
+		  // radius scale
+		  radiusScale: 1.0,
+	  
+		  // font size
+		  fontSize: 40,
+	  
+		  // if false, max value increases automatically if value > maxValue
+		  limitMax: false,
+	  
+		  // if true, the min value of the gauge will be fixed
+		  limitMin: false,
+	  
+		  // High resolution support
+		  highDpiSupport: true
+    };
+
+    var opts_humidity = {
+		// options here
+
+		// color configs
+		colorStart: "#6fadcf",
+		colorStop: void 0,
+		gradientType: 0,
+		strokeColor: "#e0e0e0",
+		generateGradient: true,
+		percentColors: [[0.0, "#a9d70b" ], [0.50, "#f9c802"], [1.0, "#ff0000"]],
+
+		// customize pointer
+		pointer: {
+			length: 0.8,
+			strokeWidth: 0.035,
+			iconScale: 1.0
+		  },
+
+		   // static labels
+		   staticLabels: {
+			font: "10px sans-serif",
+			labels: [0, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+			fractionDigits: 0
+		  },
+		   // static zones
+		   staticZones: [
+			{strokeStyle: "#F03E3E", min: 0, max: 40},
+			{strokeStyle: "#30B32D", min: 40, max: 70},
+			{strokeStyle: "#3DAEF0", min: 70, max: 100}
+		  ],
+		   // render ticks
+		   renderTicks: {
+			divisions: 5,
+			divWidth: 1.1,
+			divLength: 0.7,
+			divColor: "#333333",
+			subDivisions: 3,
+			subLength: 0.5,
+			subWidth: 0.6,
+			subColor: "#666666"
+		  },
+	  
+		  // the span of the gauge arc
+		  angle: -0.05,
+	  
+		  // line thickness
+		  lineWidth: 0.44,
+	  
+		  // radius scale
+		  radiusScale: 1.0,
+	  
+		  // font size
+		  fontSize: 40,
+	  
+		  // if false, max value increases automatically if value > maxValue
+		  limitMax: false,
+	  
+		  // if true, the min value of the gauge will be fixed
+		  limitMin: false,
+	  
+		  // High resolution support
+		  highDpiSupport: true
+    };
+    
+	var target = document.getElementById('gauge_temperature');
+	var gauge = new Gauge(target).setOptions(opts_temperature);
+	document.getElementById("preview-textfield").className = "preview-textfield";
+    gauge.setTextField(document.getElementById("preview-textfield"));
+    
+	gauge.maxValue = 50;
+	gauge.setMinValue(0);
+	gauge.set(20);
+	
+    gauge.animationSpeed = 32
+
+    var target = document.getElementById('gauge_humidity');
+	var gauge = new Gauge(target).setOptions(opts_humidity);
+	document.getElementById("preview-textfield2").className = "preview-textfield2";
+	gauge.setTextField(document.getElementById("preview-textfield2"));
+	gauge.maxValue = 100;
+	gauge.setMinValue(0);
+	gauge.set(50);
+	
+    gauge.animationSpeed = 32
+    ///////////////////////////////////////
+
+
 })(jQuery); // End of use strict
