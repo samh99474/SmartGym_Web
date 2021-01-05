f26967d05c7a46c1e8b3519ead7623931fc86d4d
106360130 <bs25017@gmail.com>
2021-01-06 01:59:42 +0800

    已將測試資料都上傳至OM2M，待寫測試頁面

--- routes/CreateContent.js
+++ routes/CreateContent.js
@@ -7,6 +7,8 @@ var OM2M_URL = app_func.require_URL();
 //超全域變數
 //console.log('OM2M_URL : ' + OM2M_URL);
 
+var read_txt = require('./read_txt');  //在同一資料夾不用在寫檔案名稱
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -36,7 +38,6 @@ function creat_DESCRIPTOR_contentInstance(body){
               &lt;str name=&quot;User_Name&quot; val=&quot;${body.User_Name}&quot;/&gt;
               &lt;str name=&quot;Date&quot; val=&quot;${body.Date}&quot;/&gt;
               &lt;str name=&quot;Start_Time&quot; val=&quot;${body.Start_Time}&quot;/&gt;
-              &lt;int name=&quot;Date&quot; val=&quot;${body.Date}&quot;/&gt;
               &lt;int name=&quot;End_Time&quot; val=&quot;${body.End_Time}&quot;/&gt;
               &lt;int name=&quot;Weight&quot; val=&quot;${body.Weight}&quot;/&gt;
               &lt;int name=&quot;number_of_set&quot; val=&quot;${body.number_of_set}&quot;/&gt;
@@ -51,8 +52,43 @@ function creat_DESCRIPTOR_contentInstance(body){
     var res = request('POST', `${OM2M_URL}~/mn-cse/mn-name/${body.input_postMachine_Use}/DATA` , {headers:headers , body:xml});
     //var res = request('POST', 'http://localhost:8080/~/mn-cse/CAE447774024' , {headers:headers , body:xml});
 
-    console.log(res.getBody('utf-8'));
+    //console.log(res.getBody('utf-8'));
   }
 
+
+    //上傳測試資料
+  
+  //debug用
+  var read_data = fs.readFileSync('D://JeffTaipeiTech2020//i_GYM//test_data.txt', 'utf-8');
+  var data_str_split = read_txt.split_data(read_data);
+  //console.log('str_test_split : ' + typeof(str_test_split));
+  var input; 
+
+  //查看object個數
+  data_str_split_2 = data_str_split.toString().split("\n");
+  var test_amount = Object.keys(data_str_split_2);
+  var test_len = test_amount.length;
+  console.log('test_len : ' + test_len);
+  //查看object個數
+
+  for(var i = 0; i < test_len-1 ; i = i+10)
+  {
+      input = read_txt.data_content(data_str_split[0], i);
+      /*
+      console.log('User_Name : ' + input.User_Name);
+      console.log('Date : ' + input.Date);
+      console.log('Start_Time : ' + input.Start_Time);
+      console.log('End_Time : ' + input.End_Time);
+      console.log('Weight : ' + input.Weight);
+      console.log('Reps : ' + input.Reps);
+      console.log('number_of_set : ' + input.number_of_set);
+      console.log('Average_speed : ' + input.Average_speed);
+      console.log('Calories : ' + input.Calories + '\n');
+      */
+
+     creat_DESCRIPTOR_contentInstance(input);
+  }
+  
+  //上傳測試資料
 
   module.exports = router;
