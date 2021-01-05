2a9d9fe71827faccdcf4cc5488c1214863f358f4
106360130 <bs25017@gmail.com>
2021-01-05 14:44:56 +0800

    加入讀取txt檔函式庫

--- routes/super_global.js
+++ routes/super_global.js
@@ -1,7 +1,7 @@
 //OM2M全域變數
 function require_URL()
 {
-  var my_OM2M_URL = 'http://jeff.frp.morrisnet.top/';
+  var my_OM2M_URL = 'http://jeff.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
   return my_OM2M_URL;
 }
 //OM2M全域變數
