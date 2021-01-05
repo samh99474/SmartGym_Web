ef2b429596b9e2d933aedae62595b715fdd5550b
cjh8817676 <jonathan1446171@gmail.com>
2021-01-05 23:03:30 +0800

    rebase2

--- routes/super_global.js
+++ routes/super_global.js
@@ -1,18 +1,11 @@
 //OM2M全域變數
 function require_URL()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
   var my_OM2M_URL = 'http://jeff.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
-=======
-  var my_OM2M_URL = 'http://localhost:8080/';
->>>>>>> ea44b16 (條一個參數即可決定m2m網址)
-=======
-  var my_OM2M_URL = 'http://flion.frp.morrisnet.top/';
->>>>>>> 0a7dcc941af09424b052f9e83336de98f7a0e76c
   return my_OM2M_URL;
 }
 //OM2M全域變數
+
 
 module.exports = {
     require_URL : require_URL
