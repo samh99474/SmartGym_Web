7d1a26024cb262b18a95d139e2ac44a274853830
cjh8817676 <jonathan1446171@gmail.com>
2021-01-05 22:58:08 +0800

    jonathan rebase

--- routes/super_global.js
+++ routes/super_global.js
@@ -1,7 +1,11 @@
 //OM2M全域變數
 function require_URL()
 {
+<<<<<<< HEAD
   var my_OM2M_URL = 'http://jeff.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
+=======
+  var my_OM2M_URL = 'http://localhost:8080/';
+>>>>>>> ea44b16 (條一個參數即可決定m2m網址)
   return my_OM2M_URL;
 }
 //OM2M全域變數
