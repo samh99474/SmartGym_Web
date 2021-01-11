48ae1b5a9813df09f468e77e04edfb9e7f36e28a
106360130 <bs25017@gmail.com>
2021-01-12 04:34:35 +0800

    更改成OM2M網址

--- routes/super_global.js
+++ routes/super_global.js
@@ -1,13 +1,13 @@
 //OM2M全域變數
 function require_URL()
 {
-  var my_OM2M_URL = 'http://flion.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
+  var my_OM2M_URL = 'http://jeff.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
   return my_OM2M_URL;
 }
 //OM2M全域變數
 function require_Email()
 {
-  var email = 'http://flion.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
+  var email = 'http://jeff.frp.morrisnet.top/';  //可以根據自己的OM2M去做更改
   return email;
 }
 
